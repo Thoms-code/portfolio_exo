bd085c9d8229e09d1a79626db2bb76aac1eeebb1
Thoms-code <catalanothomas@outlook.fr>
2020-01-24 11:06:46 +0100

    clear 2

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React, { Fragment } from 'react';
+
+const App = () => {
+
+
+    return (
+        <Fragment>
+            <h1>HELLO</h1>
+        </Fragment>
+    )
+}
+
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Header = () => {
+
+
+    return (
+        <header id="header">
+            <h1>Catalano Thomas</h1>
+        </header>
+    )
+}
+
+
+export default Header;
