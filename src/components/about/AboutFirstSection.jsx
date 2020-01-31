169be64aa2aa8027435400c8471422cf8ef0f4cf
Thoms-code <catalanothomas@outlook.fr>
2020-01-31 15:37:22 +0100

    Beta 0.1

--- src/components/about/AboutFirstSection.jsx
+++ src/components/about/AboutFirstSection.jsx
@@ -6,7 +6,7 @@ const AboutFirstSection = () => {
         <section id="about_home_big">
             <section id="about_home">
                 <section className="about_title">
-                    <h1>VAL<br/>LEURS<span>.</span></h1>
+                    <h1>VA<br/>LEURS<span>.</span></h1>
                     <h1 id="about_title-mobile">VALEURS<span>.</span></h1>
                 </section>
             </section>
